6f8ef015924e2371c87e9fd44ac80bef2f03402c
AronazGithubz <a.finocenka.lubuntu@gmail.com>
2024-10-24 21:48:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ read -p "What package manager do you use? (Example: apt, yum):" pkg
 echo "Installing necessary packages.."
 sudo $pkg install figlet
 sudo $pkg install neofetch
-
+wget https://raw.githubusercontent.com/AronasGITHUB/cterm/refs/heads/main/cterm.sh
 echo "Applying cterm.."
-c
+chmod +x cterm.sh
+echo "./cterm.sh" >> ".bashrc"
+echo "Get ready to restart your terminal, 3 seconds left"
+sleep 3
+source ~/.bashrc
